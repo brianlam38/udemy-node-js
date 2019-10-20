b64872432d22c7b383a093209692c37d08430a7d
brianlam38 <brian.lam38@gmail.om>
2019-10-20 20:25:16 +1100

    Updating content

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,13 @@
 // 3.8: Node.js core modules
 const fs = require('fs')
-
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
 fs.appendFileSync('notes.txt', '\nBrian is awesome.')
 
 // 3.9: Importing local files
-const checkUtils = require('./src/utils.js')
+const add = require('./src/utils.js')
+const sum = add(4, -2)
+console.log(sum)
 
-checkUtils()
-
+const getNotes = require('./src/notes.js')
+const message = getNotes()
+console.log(message)
--- notes-app/src/utils.js
+++ notes-app/src/utils.js
@@ -1,2 +1,7 @@
 // Contains useful utilities for the rest of the application.
+console.log('Running utils.js')
 
+const add = function (a, b) {
+    return a + b
+}
+module.exports = add
