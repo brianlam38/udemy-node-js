8bb1d817078195ef9c27f03a68abe67ef5b2da3e
brianlam38 <brian.lam38@gmail.om>
2019-10-24 14:22:39 +1100

    Adding fs and cmdline notes and yargs module.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
-// 3.8: Node.js core modules
+// Node.js core modules
 const fs = require('fs')
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
 fs.appendFileSync('notes.txt', '\nBrian is awesome.')
 
-// 3.9: Importing local files
+// Importing local files
 const add = require('./src/utils.js')
 const sum = add(4, -2)
 console.log(sum)
@@ -12,12 +12,13 @@ const getNotes = require('./src/notes.js')
 const message = getNotes()
 console.log(message)
 
-// 4.0: Importing npm modules
+// Importing npm modules
 const validator = require('validator')
 console.log(validator.isEmail('brian@lol.com')) // valid email
 console.log(validator.isEmail('brianlol.com'))  // invalid email
 console.log(validator.isURL('https://brianl.me'))
 
+// Printing in colour
 const chalk = require('chalk')
 const greenMessage = chalk.green('This is a message.')
 console.log(greenMessage)
@@ -25,3 +26,18 @@ const greenBoldMessage = chalk.bold(greenMessage)
 console.log(greenBoldMessage)
 const greenBoldUnderlineMessage = chalk.underline(greenBoldMessage)
 console.log(greenBoldUnderlineMessage)
+
+// You can install node modules globally using node install [module] -g
+// ^Ideally shouldn't use this and keep environment within the repository
+
+// File System and Command Line Arguments
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
+
+// Command Line Arg Parsing - Yargs
+
