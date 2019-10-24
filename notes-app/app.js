2931c8eac0e38b1885f7953be9b7076a18a7b130
brianlam38 <brian.lam38@gmail.om>
2019-10-24 15:09:13 +1100

    Updating coursework.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,9 @@
-// Node.js core modules
+// ### NODE.JS CORE MODULES
 const fs = require('fs')
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
 fs.appendFileSync('notes.txt', '\nBrian is awesome.')
 
-// Importing local files
+// ### IMPORTING LOCAL FILES
 const add = require('./src/utils.js')
 const sum = add(4, -2)
 console.log(sum)
@@ -12,13 +12,13 @@ const getNotes = require('./src/notes.js')
 const message = getNotes()
 console.log(message)
 
-// Importing npm modules
+// ### IMPORTING NPM MODULES
 const validator = require('validator')
 console.log(validator.isEmail('brian@lol.com')) // valid email
 console.log(validator.isEmail('brianlol.com'))  // invalid email
 console.log(validator.isURL('https://brianl.me'))
 
-// Printing in colour
+// ### PRINTING IN COLOUR
 const chalk = require('chalk')
 const greenMessage = chalk.green('This is a message.')
 console.log(greenMessage)
@@ -30,7 +30,7 @@ console.log(greenBoldUnderlineMessage)
 // You can install node modules globally using node install [module] -g
 // ^Ideally shouldn't use this and keep environment within the repository
 
-// File System and Command Line Arguments
+// ### FILE SYSTEM AND COMMAND LINE ARGUMENTS
 const command = process.argv[2]
 
 if (command === 'add') {
@@ -39,5 +39,50 @@ if (command === 'add') {
     console.log('Removing note!')
 }
 
-// Command Line Arg Parsing - Yargs
+// ### COMMAND LINE ARG PARSING - YARGS
+const yargs = require('yargs')
+console.log(process.argv) // default node args processing
+console.log(yargs.argv)   // yargs node args processing
 
+// add
+yargs.version('1.1.0')
+yargs.command({
+    command: 'add', 
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+// remove
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note!',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+// list
+yargs.command({
+    command: 'list',
+    describe: 'List your notes!',
+    handler: function () {
+        console.log('Listing out all notes!')
+    }
+})
+
+yargs.parse()
+
+// ### 
